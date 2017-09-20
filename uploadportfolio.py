7e567235bfac86d6f38c7181bcd9ba7631ba3127
Greg Clark <clarkgrg@gmail.com>
2017-09-19 22:08:29 -0500

    Updated python script for lambda function

--- uploadportfolio.py
+++ uploadportfolio.py
@@ -3,16 +3,21 @@ import boto3
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
 
-build_bucket = s3.Bucket('portfoliobuild.excelapis.com')
-portfolio_zip = io.BytesIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-portfolio_bucket = s3.Bucket('portfolio.excelapis.com')
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj,nm,
-            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
+
+    build_bucket = s3.Bucket('portfoliobuild.excelapis.com')
+    portfolio_zip = io.BytesIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    portfolio_bucket = s3.Bucket('portfolio.excelapis.com')
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj,nm,
+                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    print("Job Done")
+    return 'Hello from Lambda'
